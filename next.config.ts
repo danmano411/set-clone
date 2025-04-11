57f8951e06b24c876cfe19b37576a769039bc4ba
Dan Mano <danmano411@gmail.com>
2025-04-11 00:03:41 -0400

    used gh-pages for next

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-   /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-   output: "export",
-
-   /**
-    * Set base path. This is the slug of your GitHub repository.
-    *
-    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-    */
-   basePath: "/nextjs-github-pages",
- 
-   /**
-    * Disable server-based image optimization. Next.js does not support
-    * dynamic features with static exports.
-    *
-    * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-    */
-   images: {
-     unoptimized: true,
-   },
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Needed if you use <Image> component
+  },
+  basePath: '/set-clone', // Important for GitHub Pages!
+  assetPrefix: '/set-clone',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
